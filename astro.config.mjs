652e90c211bb504ef8e748bb702682f9d1780222
Tanner Berman <berman.tanner@gmail.com>
2025-11-16 21:11:46 -0800

    fixed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://bermantanner.github.io",
   base: "/CSE134B-HW4-ASTRO/",
-  outDir: "docs", // 👈 key change for GitHub Pages
+  outDir: "docs", 
 });
